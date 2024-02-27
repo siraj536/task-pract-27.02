5e707961cc464c0b0fe814fd801e3e930d7379a6
Mammadov Siraj <sirajnm@code.edu.az>
2024-02-27 13:01:24 +0400

    fdfd

---
+++ practic/User.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace practic
+{
+    internal class User
+    {
+        public int id;
+        public string fullname;
+        public int age;
+        public string email;
+        public string password;
+
+    }
+}
---
+++ practic/UserService.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace practic
+{
+    internal class UserService
+    {
+        public User[] GetAll()
+        {
+            User user1 = new User()
+            {
+                id = 1,
+                fullname = "Elmir Qafarzade",
+                age = 21,
+                email = "Qafarzade@gmail.com",
+                password = "Elmir123"
+            };
+
+
+            User user2 = new User()
+            {
+                id = 2,
+                fullname = "Nurlan Umudov",
+                age = 22,
+                email = "Umudov@gmail.com",
+                password = "Nurlan123"
+            };
+
+
+            User user3 = new User()
+            {
+                id = 3,
+                fullname = "Sirac Mamedov",
+                age = 23,
+                email = "Mamedov@gmail.com",
+                password = "Sirac123"
+
+
+            };
+
+
+            User[] users = { user1, user2, user3 };
+            return users;
+        }
+        public User GetById(User[] users, int id)
+        {
+            return users.FirstOrDefault(m => m.id == id);
+
+        }
+
+    }
+   
+
+
+    
+        
+
+    
+}
